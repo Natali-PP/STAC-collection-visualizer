6a23641c213025e438aa2635be6e9ef3bd0fcd91
Natali Palacio Pastor <cristinanatalip@gmail.com>
2024-03-24 22:20:43 -0300

    Added home page form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,117 +1,116 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import React, { useEffect, useState } from "react";
+import Datepicker from "react-tailwindcss-datepicker";
+import useCollectionStore from "@/store/collectionsStore";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+import GlobeAnimation from "@/components/GlobeAnimation/GlobeAnimation";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [errorCollectionId, setErrorCollectionId] = useState(false);
+  const [errorSTACServerUrl, setErrorSTACServerUrl] = useState(false);
+  const [disableButton, setDisableButton] = useState(false);
+  const updateDateRange = useCollectionStore((store) => store.updateDateRange);
+  const dateRange = useCollectionStore((store) => store.dateRange);
+  const collectionId = useCollectionStore((store) => store.collectionId);
+  const updateCollectionId = useCollectionStore(
+    (store) => store.updateCollectionId
+  );
+  const STACServerUrl = useCollectionStore((store) => store.STACServerUrl);
+  const updateSTACServerUrl = useCollectionStore(
+    (store) => store.updateSTACServerUrl
+  );
+  const router = useRouter();
+
+  useEffect(() => {
+    collectionId !== "landsat8_c2l1t1"
+      ? setErrorCollectionId(true)
+      : setErrorCollectionId(false);
+  }, [collectionId]);
+
+  useEffect(() => {
+    STACServerUrl !== "https://eod-catalog-svc-prod.astraea.earth"
+      ? setErrorSTACServerUrl(true)
+      : setErrorSTACServerUrl(false);
+  }, [STACServerUrl]);
+
+  const handleGetResults = () => {
+    if (!errorSTACServerUrl && !errorCollectionId) router.push("/result");
+  };
+
+  useEffect(() => {
+    errorSTACServerUrl || errorCollectionId
+      ? setDisableButton(true)
+      : setDisableButton(false);
+  }, [disableButton, errorCollectionId, errorSTACServerUrl]);
+
+  useEffect(() => {
+    console.log("date", dateRange);
+  }, [dateRange]);
+
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex flex-col items-center justify-center ${inter.className} h-screen`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="absolute top-0 left-0" style={{ zIndex: "-1" }}>
+        <GlobeAnimation width={"800px"} height={"800px"} />
+      </div>
+      <div
+        className={`flex flex-col items-center justify-center gap-6 w-1/2 py-6 shadow-xl border-2 bg-slate-50`}
+      >
+        <h1>Type here the STAC server URL you want to access</h1>
+        <input
+          className={`border-2 rounded w-3/4 py-2.5 pl-4 pr-14 focus:outline-none focus:ring-0${
+            errorSTACServerUrl
+              ? " border-red-500 focus:border-red-500 "
+              : " border-slate-300 focus:border-blue-500"
+          }`}
+          placeholder="https://eod-catalog-svc-prod.astraea.earth"
+          value={STACServerUrl}
+          onChange={(e) => updateSTACServerUrl(e.target.value)}
+        ></input>
+        {errorSTACServerUrl ? (
+          <p className="text-sm text-red-700">
+            Enter the valid STAC Server URL
+          </p>
+        ) : null}
+        <h1>Type here the collection ID</h1>
+        <input
+          className={`border-2 w-3/4 rounded py-2.5 pl-4 pr-14 focus:outline-none focus:ring-0${
+            errorCollectionId
+              ? " border-red-500 focus:border-red-500 "
+              : " border-slate-300 focus:border-blue-500"
+          }`}
+          placeholder="landsat8_c2l1t1"
+          value={collectionId}
+          onChange={(e) => updateCollectionId(e.target.value)}
+        ></input>
+        {errorCollectionId ? (
+          <p className="text-sm text-red-700">Enter the valid collection ID</p>
+        ) : null}
+        <h1>Choose a date range to get your data</h1>
+        <div className="w-3/4">
+          <Datepicker
+            value={dateRange}
+            onChange={updateDateRange}
+            displayFormat={"DD/MM/YYYY"}
+            inputClassName={`${inter.className} border-slate-300 border-2 rounded w-full py-2.5 pl-4 pr-14`}
+            startFrom={dateRange.startDate}
+            //isForwardLooking={false}
+          />
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          disabled={false}
+          onClick={handleGetResults}
+          className={`${
+            disableButton
+              ? "bg-slate-400 text-white "
+              : "bg-blue-500 text-white "
+          }w-3/4 py-2.5 rounded text-center`}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Get Results!
+        </button>
       </div>
     </main>
   );
