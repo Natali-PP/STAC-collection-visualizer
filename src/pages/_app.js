76f6cb91f4f4815da681636d8870603ffb8fdb31
Natali Palacio Pastor <cristinanatalip@gmail.com>
2024-03-24 22:20:13 -0300

    Added page transition component

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import Layout from "@/components/Layout/Layout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  return (
+    <Layout>
+      <Component {...pageProps} key={router.route} />
+    </Layout>
+  );
 }
